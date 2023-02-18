7f9cb9bf1e38c9b000728c037c7790c9bbe56c84
Karl Krauth <karl.krauth@gmail.com>
2023-02-18 13:07:25 -0800

    Add support for python 3.7, bump version to 0.1.6.

--- src/magnify/__init__.py
+++ src/magnify/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.6"
 
 from magnify.assay import Assay
 from magnify.find import find_buttons
--- src/magnify/assay.py
+++ src/magnify/assay.py
@@ -2,16 +2,30 @@ import numpy as np
 
 
 class Assay:
+    # The type of assay that we imaged can be "bead" or "chip".
+    # In a chip assay the items we are imaging are buttons, whereas
+    # in a bead assay we are imaging beads.
     type: str
-    ids: np.ndarray
+    # The names of each item. Names do not have to be unique.
+    # This is a num_row x num_col array for chip assays,
+    # and a num_item array for bead assays.
+    names: np.ndarray
+    # The time in seconds at which each image was taken.
     times: np.ndarray
-    channels: list[str]
-    info: dict
+    # The channels each image was acquired in.
+    channels: np.ndarray
 
+    # The images acquired for this assay.
+    # This is a num_times x num_channels x image_height x image_width array.
     images: np.ndarray
-    regions: np.ndarray
-    offsets: np.ndarray
+    # The center of each item in row-col coordinates.
+    # This is a num_times x num_channels x num_items x 2 array.
     centers: np.ndarray
+    # Subsets of the images array that contains items
+    regions: np.ndarray
+    # The row/column offsets of each region in the 
+    offsets: np.ndarray
+
     fg: np.ndarray
     bg: np.ndarray
     valid: np.ndarray
