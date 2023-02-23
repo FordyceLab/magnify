dcebdec39cb9bf0b14fa81130325eae54a03b2ad
Karl Krauth <karl.krauth@gmail.com>
2023-02-22 20:22:07 -0800

    Add generalized pipeline class.

--- src/magnify/__init__.py
+++ src/magnify/__init__.py
@@ -12,5 +12,4 @@ from magnify.assay import Assay
 from magnify.find import find_buttons
 from magnify.segment import segment_buttons
 from magnify.stitch import overlap_stitch
-from magnify.pipeline import pipe
 from magnify.preprocess import basic_correct_tiles
--- src/magnify/pipeline.py
+++ src/magnify/pipeline.py
@@ -1,64 +1,49 @@
-from collections.abc import Sequence
-from typing import Any, cast
+from typing import Callable
 
-import numpy as np
-import numpy.ma as ma
-import tifffile
-import tqdm
+from numpy.typing import ArrayLike
 
-from magnify import utils
-import magnify
+from magnify.assay import Assay
 
 
-def pipe(
-    tile_paths: Sequence,
-    times: np.ndarray,
-    channels: np.ndarray,
-    search_on: str = "egfp",
-    drop_images: bool = False,
-    progress_bar: bool = False,
-    **kwargs: dict[str, Any],
-) -> magnify.Assay:
-    find_kwargs = utils.valid_kwargs(kwargs, magnify.find_buttons)
-    segment_kwargs = utils.valid_kwargs(kwargs, magnify.segment_buttons)
-    assay = magnify.Assay()
-    assay.times = times
-    assay.channels = channels
-    assays = []
-    for time_paths in tqdm.tqdm(tile_paths, disable=not progress_bar):
-        image_list = []
-        for channel_paths in time_paths:
-            tile_lists: list[list[np.ndarray]] = []
-            for row_paths in channel_paths:
-                tile_lists.append([])
-                for tile in row_paths:
-                    tile_lists[-1].append(tifffile.imread(tile))
-            tiles = np.array(tile_lists)
-            tiles = np.flip(tiles, axis=1)
-            image_list.append(magnify.overlap_stitch(tiles))
+class Pipeline:
+    def __init__(self):
+        self.loader: Callable[[ArrayLike | str], Assay] = []
+        self.components: Callable[[Assay], Assay] = []
 
-        images = np.array(image_list)
-        a = magnify.find_buttons(images, channels, search_on=search_on, **find_kwargs)
-        a = magnify.segment_buttons(a, search_on=search_on, **segment_kwargs)
-        if drop_images:
-            a.images = None
-        assays.append(a)
+    def __call__(self, data: ArrayLike | str) -> Assay:
+        assays = []
+        for assay in self.loader(data):
+            for name, component in self.components:
+                assay = component(assay)
+            assays.append(assay)
 
-    if not drop_images:
-        assay.images = np.concatenate([cast(np.ndarray, a.images)[np.newaxis] for a in assays])
-    assay.regions = np.concatenate([a.regions[np.newaxis] for a in assays])
-    assay.offsets = np.concatenate([a.offsets[np.newaxis] for a in assays])
-    assay.centers = np.concatenate([a.centers[np.newaxis] for a in assays])
-    assay.fg = ma.array(
-        assay.regions,
-        mask=np.concatenate([ma.getmask(a.fg)[np.newaxis] for a in assays]),
-        copy=False,
-    )
-    assay.bg = ma.array(
-        assay.regions,
-        mask=np.concatenate([ma.getmask(a.bg)[np.newaxis] for a in assays]),
-        copy=False,
-    )
-    assay.valid = np.concatenate([a.valid[np.newaxis] for a in assays])
+        return Assay.concat_time(assays)
 
-    return assay
+    def add_pipe(self, name: str) -> None:
+        self.components.append((name, Pipeline.get_component(name)))
+
+    registered_components: dict[str, Callable[[Assay], Assay]] = {}
+    registered_factories: dict[str, Callable[[], Callable[[Assay], Assay]]] = {}
+
+    @classmethod
+    def get_component(cls, name):
+        if name in cls.registered_components:
+            return cls.registered_components[name]
+        else:
+            return cls.registered_factories[name]()
+
+    @classmethod
+    def factory(cls, name):
+        def decorator(func: Callable[[Assay], Assay]):
+            cls.registered_factories[name] = func
+            return func
+
+        return decorator
+
+    @classmethod
+    def component(cls, name: str):
+        def decorator(func: Callable[[Assay], Assay]):
+            cls.registered_components[name] = func
+            return func
+
+        return decorator
