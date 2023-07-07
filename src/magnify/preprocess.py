d29ca8f3e26d493bd76bd249870d78c119e652db
Karl Krauth <karl.krauth@gmail.com>
2023-07-07 12:09:24 -0700

    Temporarily remove basicpy dependency.

--- src/magnify/preprocess.py
+++ src/magnify/preprocess.py
@@ -2,26 +2,11 @@ from __future__ import annotations
 from typing import cast
 import os
 
-import basicpy
 import tifffile
 import numpy as np
 import xarray as xr
 
 import magnify.registry as registry
-
-
-class Preprocessor:
-    def __call__(self, assay: xr.Dataset) -> xr.Dataset:
-        tiles = assay.image
-        flat_tiles = assay.image.reshape(-1, tiles.dim["im_row"], tiles.dim["im_col"])
-        model = basicpy.basicpy.BaSiC(get_darkfield=True, smoothness_flatfield=1)
-        result = cast(np.ndarray, model.fit_transform(flat_tiles, timelapse=False))
-        assay = assay.assign(image=result.reshape(*tiles.shape))
-        return assay
-
-    @registry.components.register("preprocess")
-    def make():
-        return Preprocessor()
 
 
 @registry.component("flatfield_correct")
