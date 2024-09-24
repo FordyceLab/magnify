9aa702b385b92b95cdc0f14bf55de6dcaba220f1
Karl Krauth <karl.krauth@gmail.com>
2024-09-24 14:55:45 -0700

    Add cluster plot utils for mrbles.

--- src/magnify/plot/__init__.py
+++ src/magnify/plot/__init__.py
@@ -1,11 +1,13 @@
 __all__ = [
     "imshow",
+    "mrbles_clusters",
     "ndplot",
     "relplot",
     "roishow",
     "set_style",
 ]
 from magnify.plot.image import roishow, imshow
+from magnify.plot.mrbles import mrbles_clusters
 from magnify.plot.ndplot import ndplot
 from magnify.plot.relation import relplot
 from magnify.plot.style import set_style
