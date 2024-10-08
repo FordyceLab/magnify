36d45c8df255a4c1be9e64dd642c1ea73bf0bff0
Karl Krauth <karl.krauth@gmail.com>
2024-10-08 15:36:30 -0700

    Change __init__.py to not use global import.

--- src/magnify/__init__.py
+++ src/magnify/__init__.py
@@ -1,14 +1,55 @@
 __version__ = "0.9.0"
 
-__all__ = []
+__all__ = [
+    "mini_chip",
+    "mini_chip_pipe",
+    "pc_chip",
+    "pc_chip_pipe",
+    "ps_chip",
+    "ps_chip_pipe",
+    "mrbles",
+    "mrbles_pipe",
+    "beads",
+    "beads_pipe",
+    "image",
+    "image_pipe",
+    "save",
+    "load",
+    "accessor",
+    "filter",
+    "find",
+    "identify",
+    "postprocess",
+    "preprocess",
+    "reader",
+    "stitch",
+]
 
-from magnify.file import *
-from magnify.registry import *
-import magnify.accessor
-import magnify.filter
-import magnify.find
-import magnify.identify
-import magnify.postprocess
-import magnify.preprocess
-import magnify.reader
-import magnify.stitch
+from .file import (
+    save,
+    load,
+)
+
+from .registry import (
+    mini_chip,
+    mini_chip_pipe,
+    pc_chip,
+    pc_chip_pipe,
+    ps_chip,
+    ps_chip_pipe,
+    mrbles,
+    mrbles_pipe,
+    beads,
+    beads_pipe,
+    image,
+    image_pipe,
+)
+
+from . import accessor
+from . import filter
+from . import find
+from . import identify
+from . import postprocess
+from . import preprocess
+from . import reader
+from . import stitch
