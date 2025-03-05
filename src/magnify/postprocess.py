9a0dadb80d0f593ee889216cebf5be67eadfa31c
Karl Krauth <karl.krauth@gmail.com>
2025-03-05 12:06:19 -0800

    Remove unused circularize function.

--- src/magnify/postprocess.py
+++ src/magnify/postprocess.py
@@ -27,27 +27,3 @@ def drop(
         return assay.drop_vars(["tile", "tile_row", "tile_col"], errors="ignore")
     else:
         return assay
-
-
-@registry.component("circularize")
-def circularize(
-    assay: xr.Dataset,
-):
-    subassay = assay.isel(time=0, channel=0)
-    fg = subassay.fg.to_numpy()
-    bg = subassay.bg.to_numpy()
-    for i in range(assay.sizes["mark"]):
-        old_fg = utils.to_uint8(fg[i])
-        contour = cv.findContours(old_fg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0][0]
-        center, radius = cv.minEnclosingCircle(contour)
-        fg[i] = utils.circle(
-            fg.shape[-1],
-            row=round(center[1]),
-            col=round(center[0]),
-            radius=round(radius),
-            value=True,
-        )
-        bg[i] = ~fg[i] & bg[i]
-    assay.fg[:] = fg[:, np.newaxis, np.newaxis, :, :]
-    assay.bg[:] = bg[:, np.newaxis, np.newaxis, :, :]
-    return assay
