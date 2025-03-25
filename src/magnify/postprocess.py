a70536588523137f2b97ef63758c0c806d1b6886
Karl Krauth <karl.krauth@gmail.com>
2025-03-25 09:28:46 -0700

    Remove test print (last commit solved restore_format squeeze bug).

--- src/magnify/postprocess.py
+++ src/magnify/postprocess.py
@@ -29,7 +29,6 @@ def restore_format(xp: xr.Dataset | xr.DataArray) -> xr.Dataset | xr.DataArray:
     standard_dims = ["channel", "time", "tile_row", "tile_col", "tile_y", "tile_x"]
     for dim in standard_dims:
         if dim not in xp.__original_tile_dims__ and dim in xp.dims:
-            print(xp)
             xp = xp.squeeze(dim)
 
     # Restore the original dimension order in the context of dimensions that didn't exist before.
